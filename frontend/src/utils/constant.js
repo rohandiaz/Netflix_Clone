ecdbdae32d2350626f774071124431c17e9198ba
rohandiaz <rohanamylasedas@gmail.com>
2024-12-26 13:33:54 +0530

    Added movie hooks for fetching

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -7,3 +7,13 @@ export const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOGQzZDFmYmU5NzQ0M2IwYTRhODc0YzE3YzFhMzFkOCIsIm5iZiI6MTczNDk4NDk1MC44MSwic3ViIjoiNjc2OWM0ZjYxYmI5MTMxMGMwNjRhMDdlIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.-yZoudCu0h7aQhzsVrhJENLW00_NmmUgQ7nn1B5ihG8'
     }
   };
+
+
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
