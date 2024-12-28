cf1d780c29f0de01860343e1473abba752942aab
rohandiaz <rohanamylasedas@gmail.com>
2024-12-28 13:52:53 +0530

    Update CORS settings to allow requests from new production URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://netflix-clone-1-b9iq.onrender.com',
+    origin: 'https://netflix-2-0-ywat.onrender.com',
     credentials: true
 }
 app.use(cors(corsOptions));
