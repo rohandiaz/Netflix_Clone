e530a65d1184560d58bb4a0c52d97b3d9c5e00ce
rohandiaz <rohanamylasedas@gmail.com>
2024-12-28 13:41:02 +0530

    Update API endpoint to new production URL

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "https://netflix-clone-918a.onrender.com/api/v1/user";
+export const API_END_POINT = "https://netflix-2-0-a5gv.onrender.com/api/v1/user";
 
 export const options = {
     method: 'GET',
